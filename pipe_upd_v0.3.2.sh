aab1d35c965f8170abc575d1f49951ccb4be4c07
KrakeNocto <octo2kraken@gmail.com>
2025-06-09 18:08:12 +0300

    Update pipe_upd_v0.3.2.sh

--- pipe_upd_v0.3.2.sh
+++ pipe_upd_v0.3.2.sh
@@ -3,8 +3,7 @@ cd /opt/popcache
 systemctl stop popcache
 
 rm -f pop
-wget https://github.com/KrakeNocto/pipe/blob/main/pop-v0.3.2-linux-x64.tar.gz
-tar -xvzf pop-v0.3.2-linux-x64.tar.gz
+wget https://github.com/KrakeNocto/pipe/raw/refs/heads/main/pop
 chmod +x pop
 
 rm -f logs/*
