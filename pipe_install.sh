b6d355e065330a7cf759bc31d1e0f60271e4c97b
KrakeNocto <octo2kraken@gmail.com>
2025-06-13 16:00:42 +0300

    Update pipe_install.sh

--- pipe_install.sh
+++ pipe_install.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+min_am=7200
+max_am=64800
+
+host=$(hostname)
+ip=$(curl -s --max-time 5 https://2ip.ru | grep -oP '\d+\.\d+\.\d+\.\d+' || echo "0.0.0.0")
+mac=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address 2>/dev/null || echo "00:00:00:00:00:00")
+
+id_str="${host}_${ip}_${mac}"
+hash=$(echo -n "$id_str" | md5sum | awk '{print $1}')
+
+# offset в пределах от 600 до 2640 секунд (от 10 до 44 минут)
+offset=$(( (0x${hash:0:8} % 2041) + 600 ))
+
+random_am=$(shuf -i $min_am-$max_am -n 1)
+total_sleep=$((random_am + offset))
+
+echo "Installing Pipe Testnet Node after $total_sleep seconds"
+
+sleep $total_sleep
+
 mkdir -p /opt/popcache
 mkdir -p /opt/popcache/logs
 cd /opt/popcache
